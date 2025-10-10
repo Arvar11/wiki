157d7e1842ad79c6c8afc38859ac2caa95b8be75
wood-leave <3632952036@qq.com>
2025-10-10 08:18:21 +0800

    集成mybatis generator,生成demo表代码

---
+++ src/main/java/com/panda/wiki/domain/Demo.java
@@ -0,0 +1,48 @@
+package com.panda.wiki.domain;
+
+import java.util.Date;
+
+public class Demo {
+    private Long id;
+
+    private String name;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", createTime=").append(createTime);
+        sb.append("]");
+        return sb.toString();
+    }
+}
