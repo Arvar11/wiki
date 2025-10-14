9d7d1fff0f151535c26cf4144cc234ec502c30b9
wood-leave <3632952036@qq.com>
2025-10-14 14:34:15 +0800

    对电子书查询和保存做参数校验集成Validation，页码不为空，条数有上限，保存名称不为空

--- src/main/java/com/panda/wiki/controller/EbookController.java
+++ src/main/java/com/panda/wiki/controller/EbookController.java
@@ -9,6 +9,7 @@ import com.panda.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping ("/ebook")
@@ -18,7 +19,7 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(EbookQueryReq req) {
+    public CommonResp list( @Valid EbookQueryReq req) {
         PageResp<EbookResp> list = ebookService.list(req);
         CommonResp<PageResp<EbookResp>> resp = new CommonResp<>();
         resp.setContent(list);
@@ -26,7 +27,7 @@ public class EbookController {
     }
 
     @PostMapping("/save")
-    public CommonResp save(@RequestBody EbookSaveReq req) {
+    public CommonResp save(@Valid @RequestBody EbookSaveReq req) {
         CommonResp resp = new CommonResp<>();
         ebookService.save(req);
         return resp;
--- src/main/java/com/panda/wiki/req/PageReq.java
+++ src/main/java/com/panda/wiki/req/PageReq.java
@@ -1,8 +1,14 @@
 package com.panda.wiki.req;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 public class PageReq {
+    @NotNull(message="【页码】不能为空")
     private int page;
 
+    @NotNull(message = "每页条数不能为空")
+    @Max(value = 100,message = "每页条数不超过100")
     private int size;
 
     public int getSize() {
