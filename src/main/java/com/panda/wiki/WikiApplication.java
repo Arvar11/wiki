d6ea339b49af1728242231cdf34b98da8ad36fb0
wood-leave <3632952036@qq.com>
2025-10-18 17:02:04 +0800

    定时任务示例

--- src/main/java/com/panda/wiki/WikiApplication.java
+++ src/main/java/com/panda/wiki/WikiApplication.java
@@ -3,7 +3,9 @@ package com.panda.wiki;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling//定时任务
 @SpringBootApplication
 @MapperScan("com.panda.wiki.mapper")
 public class WikiApplication {
