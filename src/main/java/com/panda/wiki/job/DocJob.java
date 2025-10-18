55b47d49f7edbdacb1a6ba503059a0a3acc6a8cf
wood-leave <3632952036@qq.com>
2025-10-18 20:08:15 +0800

    增加定时器的日志（记录电子书更新耗时)

--- src/main/java/com/panda/wiki/job/DocJob.java
+++ src/main/java/com/panda/wiki/job/DocJob.java
@@ -1,14 +1,17 @@
 package com.panda.wiki.job;
 
 import com.panda.wiki.service.DocService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Component
+@Slf4j
 public class DocJob {
 
     @Resource
@@ -19,6 +22,9 @@ public class DocJob {
      */
     @Scheduled(cron = "5/30 * * * * ?")
     public void cron()  {
+        log.info("电子书信息更新开始");
+        long start=System.currentTimeMillis();
         docService.updateEbookInfo();
+        log.info("电子书更新结束，耗时:{}毫秒",System.currentTimeMillis()-start);
     }
 }
